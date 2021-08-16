da4aed5dfc47c265a781bdfba9ca17635e1ffdd7
Florian <kadjiflorian@gmail.com>
2021-08-16 15:37:44 +0100

    Ajout du dossier vehicle

---
+++ vehicle/src/main/java/org/jahagal/com/vehicle/VehicleApplication.java
@@ -0,0 +1,13 @@
+package org.jahagal.com.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleApplication.class, args);
+	}
+
+}
